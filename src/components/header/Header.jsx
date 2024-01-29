31c6fcdd39f6cebf409874e223634d918315ff2f
MinhThuan <thuanngo454@gmail.com>
2024-01-29 18:04:38 +0700

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,47 @@ import React from 'react';
 import './header.css';
 
 const Header = () => {
-    return (
-        <div>Hello world</div>
-    )
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index" className="nav__logo">Minh Thuận</a>
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="bx bx-home nav__icon"></i> Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+              <i className='bx bx-user nav__icon'></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#education" className="nav__link">
+              <i className='bx bxs-book-open nav__icon'></i> Education
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+              <i className='bx bxs-notepad nav__icon'></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#projects" className="nav__link">
+              <i className="uil uil-apps nav__icon"></i> Projects
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+              <i className='bx bx-message-dots nav__icon'></i> Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header;
